837140ceb5c7f44d8609848f188195eb54de937a
William Horn <williamjosephhorn@gmail.com>
2023-02-06 07:37:55 -0500

    checkpoint

--- src/content-scripts/content.js
+++ src/content-scripts/content.js
@@ -1,3 +1,6 @@
-console.log("Some content script");
 
-export {};
+import test from "../lib/helpers/test_module";
+
+console.log("Some content script");
+console.log(test);
+
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,5 @@
+
+import test_module from "./lib/helpers/test_module";
+
 console.log("Some service worker");
+console.log(test_module);
